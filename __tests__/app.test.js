0d147ea3a74c743b1ed0880a49c8a7e47eeba927
Lynerah <celinedeboeck@gmail>
2021-11-26 13:25:15 +0100

    suite exercice unit test palindrom

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -1,7 +1,18 @@
 const app = require('../app');
 
-describe('Palindrom', () => {
-   it('receive a sentence', () =>{
-      expect(app.hello.length).ToBeGreaterThan(0);
-   })
-})
+//test V1
+// describe('Palindrom', () => {
+//    it('receive a sentence', () =>{
+//       expect(app.hello.length).toBeGreaterThan(0);
+//    });
+//    it('have a lenght of 11', () =>{
+//       expect(app.hello.length).toEqual(11);
+//    });
+// });
+
+//TEST V2
+describe('Palindrom', ()=>{
+   it('is a palindrom', ()=>{
+      expect(app.isPalindrom('kayak')).toEqual(true);
+   });
+});
--- app.js
+++ app.js
@@ -1,5 +1,16 @@
-const hello = 'Hello World';
+//TEST V1
+// const hello = 'Hello World';
+
+// module.exports = {
+//    hello
+// }  
+
+//TEST V2
+function isPalindrom(word){
+   const reverse = word.split('').reverse();
+   return true;
+}
 
 module.exports = {
-   hello
-}
+   isPalindrom
+} 
